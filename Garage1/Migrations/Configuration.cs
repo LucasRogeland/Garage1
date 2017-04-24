1f104c5503a162bdd5a03765bed09562195dce55
Lucas Rogeland <lucas@crossfade.se>
2017-04-24 11:36:44 +0200

    Database Initialized

---
+++ Garage1/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Garage1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Garage1.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Garage1.DataAccessLayer.GarageContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Garage1.DataAccessLayer.GarageContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            context.Vehicles.AddOrUpdate(
+              p => p.RegNummer,
+              new ParkedVehicle { RegNummer = "TEN-712", VehicleType = Enums.Vehicles.Car, Manufacturer = "Volvo", Model = "XC90", Color = "Black", NumberOfWheels = 4, CheckInTime = DateTime.Now },
+              new ParkedVehicle { RegNummer = "HER-034", VehicleType = Enums.Vehicles.Car, Manufacturer = "BMW", Model = "M3", Color = "Red", NumberOfWheels = 4, CheckInTime = DateTime.Now },
+              new ParkedVehicle { RegNummer = "FND-258", VehicleType = Enums.Vehicles.Car, Manufacturer = "Saab", Model = "9-3", Color = "Blue", NumberOfWheels = 4, CheckInTime = DateTime.Now }
+            );
+            
+        }
+    }
+}
---
+++ Garage1/Models/ParkedVehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Garage1.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage1.Models
+{
+    public class ParkedVehicle
+    {
+        [Key]
+        public string RegNummer { get; set; }
+        public Vehicles VehicleType { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public int NumberOfWheels { get; set; }
+    }
+}
